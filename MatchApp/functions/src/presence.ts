da3dd4b0fa70ce960de66d0436cb78717ece956b
Govindhahari118 <fraction-wimp-gush@duck.com>
2026-09-15 22:21:07 +0530

    fix(functions): keep presence callable on v1 API

--- MatchApp/functions/src/presence.ts
+++ MatchApp/functions/src/presence.ts
@@ -1,4 +1,4 @@
-import * as functions from "firebase-functions";
+import * as functions from "firebase-functions/v1";
 import { db, requireAppCheck } from "./shared";
 
 const MIN_HEARTBEAT_INTERVAL_MS = 60_000;
