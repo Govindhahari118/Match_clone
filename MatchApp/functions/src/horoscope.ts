6138d1e5445cc788e49a44b1dba4d30b115682e6
Govindhahari118 <fraction-wimp-gush@duck.com>
2026-09-15 22:20:43 +0530

    fix(functions): keep horoscope callable on v1 API

--- MatchApp/functions/src/horoscope.ts
+++ MatchApp/functions/src/horoscope.ts
@@ -1,4 +1,4 @@
-import * as functions from "firebase-functions";
+import * as functions from "firebase-functions/v1";
 import { db, requireAppCheck } from "./shared";
 
 function isHindu(religion: unknown): boolean {
